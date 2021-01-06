0a7515a3722db791cd8a720c32a1380d6957ba87
Jamey Hart <jamey.hart@jisc.ac.uk>
2021-01-06 13:49:44 +0000

    Update infra/dev/provider.tf
    Update instances of 'censusplatform' to 'geoconvert'.

--- infra/dev/provider.tf
+++ infra/dev/provider.tf
@@ -1,14 +1,14 @@
 provider "aws" {
     version = "3.5.0"
     region = "eu-west-1"
-    profile = "ukds-census-platform"
+    profile = "ukds-geoconvert"
 }
 
 terraform {
     backend "s3" {
-        bucket                  = "ukds-census-tfstate-dev"
+        bucket                  = "ukds-geoconvert-tfstate-dev"
         key                     = "terraform.tfstate"
         region                  = "eu-west-1"
-        profile                 = "ukds-census-platform"
+        profile                 = "ukds-geoconvert"
     }
 }
