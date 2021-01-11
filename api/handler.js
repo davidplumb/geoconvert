d24d57f25c356d864844b14f7786518b98d1dc8b
Jamey Hart <jamey.hart@jisc.ac.uk>
2021-01-11 15:34:57 +0000

    Fix indentations in YAML file

--- api/handler.js
+++ api/handler.js
@@ -6,6 +6,6 @@ module.exports.helloWorld = async (event) => {
     console.log("working");
     console.log(responseJSON(200, 'asdasd', false));
     
-    return responseJSON(200, {}); 
+    return responseJSON(200, "hello"); 
 
 }
