ba1b4e22d0a6c7219b0e39477b8188d8b3e93692
Jamey Hart <jamey.hart@jisc.ac.uk>
2021-01-19 15:16:53 +0000

    Update SQL query to '= any' instead of 'in'

--- api/src/lib/db.js
+++ api/src/lib/db.js
@@ -2,7 +2,7 @@ const db = require('./client');
 
 module.exports = {
     postcode: async (postcodes) => {
-        const query = `select * from dbo.lut_postcode15jul where pcstrip15jul in ($1:csv)`;
+        const query = `select * from dbo.lut_postcode15jul where pcstrip15jul = any ($1)`;
         const res = await db.any(query, [postcodes]);
         return res;
     }
