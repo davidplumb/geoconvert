8ec56e6b490890e4c9b594e84c856b3410ac1a2b
Jamey Hart <jamey.hart@jisc.ac.uk>
2021-01-22 12:18:26 +0000

    Add postcode element if not in array

--- api/src/lib/db.js
+++ api/src/lib/db.js
@@ -7,16 +7,18 @@ module.exports = {
         
         var columns = columns || ['pcstrip15jul'];
 
+        if(!columns.includes('pcstrip15jul')) {
+            columns.push('pcstrip15jul')
+        }
+
         const data = await db.any("select ${columns:raw} from dbo.lut_postcode15jul where pcstrip15jul in (${postcodes:csv})", {
             columns: columns.map(pgp.as.name).join(),
             postcodes
         });
-        console.log(columns);
+        
         const meta = await db.any("select geoginst_lower, field_label from dbo.index_pdfield where geoginst_lower in (${columns:csv})", {
             columns
         });
-
-        console.log(columns);
 
         return {
             data,
