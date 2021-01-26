722e0df8cc702e9b6b82e82379f39066afbd0b06
Jamey Hart <jamey.hart@jisc.ac.uk>
2021-01-26 15:53:34 +0000

    Change scope of variables
    Also change some variable names and change mapping of whitespace removal from postcodes

--- api/src/lib/db.js
+++ api/src/lib/db.js
@@ -5,24 +5,21 @@ const pgp = require('pg-promise')();
 module.exports = {
     postcode: async (columns, postcodes) => {
         
-        var columns = columns || ['pcstrip15jul'];
+        const cols = columns || ['pcstrip15jul'];
 
-        if(!columns.includes('pcstrip15jul')) {
-            columns.push('pcstrip15jul')
+        if(!cols.includes('pcstrip15jul')) {
+            cols.push('pcstrip15jul')
         }
 
-        var postcodes = postcodes.map(function(e) {
-            e = e.replace(/\s+/g, '');
-            return e;
-        });
+        const pcs = postcodes.map((e) => e.replace(/\s+/g, ''));
 
-        const data = await db.any("select ${columns:raw} from dbo.lut_postcode15jul where pcstrip15jul in (${postcodes:csv})", {
-            columns: columns.map(pgp.as.name).join(),
-            postcodes
+        const data = await db.any("select ${cols:raw} from dbo.lut_postcode15jul where pcstrip15jul in (${pcs:csv})", {
+            cols: cols.map(pgp.as.name).join(),
+            pcs
         });
         
-        const meta = await db.any("select geoginst_lower, field_label from dbo.index_pdfield where geoginst_lower in (${columns:csv})", {
-            columns
+        const meta = await db.any("select geoginst_lower, field_label from dbo.index_pdfield where geoginst_lower in (${cols:csv})", {
+            cols
         });
 
         return {
