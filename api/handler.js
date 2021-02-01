5d290db7d37b1841cddce6cc65b88c3d1abadb81
Jamey Hart <jamey.hart@jisc.ac.uk>
2021-02-01 12:16:37 +0000

    Add postcodewildcard API endpoint

--- api/handler.js
+++ api/handler.js
@@ -1,13 +1,14 @@
-const {
-    responseJSON
-} = require('./src/lib/response');
-
-const {
-    postcode
-} = require('./src/lib/db');
+const { responseJSON } = require('./src/lib/response');
+const { postcode, postcodewildcard } = require('./src/lib/db');
 
 module.exports.postcode = async (event) => {
     const {columns, postcodes} = JSON.parse(event.body)
     const data = await postcode(columns, postcodes)
+    return responseJSON(200, data);
+}
+
+module.exports.postcodewildcard = async (event) => {
+    const {columns, postcodewildcards} = JSON.parse(event.body)
+    const data = await postcodewildcard(columns, postcodewildcards)
     return responseJSON(200, data);
 }
--- api/src/lib/db.js
+++ api/src/lib/db.js
@@ -1,7 +1,6 @@
 const db = require('./client');
 const pgp = require('pg-promise')();
 
-
 module.exports = {
     postcode: async (columns, postcodes) => {
         
@@ -16,6 +15,33 @@ module.exports = {
         const data = await db.any("select ${cols:raw} from dbo.lut_postcode15jul where pcstrip15jul in (${pcs:csv})", {
             cols: cols.map(pgp.as.name).join(),
             pcs
+        });
+        
+        const meta = await db.any("select geoginst_lower, field_label from dbo.index_pdfield where geoginst_lower in (${cols:csv})", {
+            cols
+        });
+
+        return {
+            data,
+            meta
+        };
+    },
+    postcodewildcard: async (columns, postcodewildcards) => {
+            
+        const cols = columns || ['pctrim'];
+
+        if(!cols.includes('pctrim')) {
+            cols.push('pctrim')
+        };
+
+        const pca = [];
+
+        postcodewildcards.forEach((postcode) => pca.push(`${postcode}`.trim()));
+
+        const data = await db.any("select ${cols:raw} from dbo.lut_postcode15jul where pctrim like any (${pca:csv})", {
+            cols: cols.map(pgp.as.name).join(),
+            // pca needs to be a nested array
+            pca: [pca]
         });
         
         const meta = await db.any("select geoginst_lower, field_label from dbo.index_pdfield where geoginst_lower in (${cols:csv})", {
